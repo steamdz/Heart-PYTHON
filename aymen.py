2f0bd86dc93f1692b10c5a1fa89f1c4fe456c8ce
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2021-10-15 00:05:34 +0200

    commit intial

---
+++ aymen.py
@@ -0,0 +1,27 @@
+#AYMEN DEV 
+
+from turtle import *
+
+color("red","red")
+
+begin_fill()
+
+pensize(3)
+
+left(50)
+
+forward(133)
+
+circle(50,200)
+
+right(140)
+
+circle(50,200)
+
+fd(133)
+
+end_fill()
+
+mainloop()
+
+
